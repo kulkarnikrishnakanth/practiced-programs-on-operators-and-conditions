ac3e831adde90a2e778dd72445694ad831790ed2
kulkarnikrishnakanth <43737224+kulkarnikrishnakanth@users.noreply.github.com>
2020-02-12 17:40:37 +0530

    Add files via upload

---
+++ problem_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 17:03:11 2020
+
+@author: DW4
+"""
+
+radius = float(input("enter radius: "))
+area = 3.14 * radius ** 2
+print("area of circle in sqcm is:",area)
---
+++ problem_4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 17:07:16 2020
+
+@author: DW4
+"""
+
+base_salary = float(input("enter base salary: "))
+experience = float(input("enter your work experience: "))
+if experience <= 5:
+    pf = 0.12
+else:
+    pf = 0.24
+
+net_salary = base_salary - (base_salary * pf)
+print("net salary recieved after deduction:",net_salary)
