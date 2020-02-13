b13fb8bea1be756f3b9955361cd33dd9add49e74
kulkarnikrishnakanth <43737224+kulkarnikrishnakanth@users.noreply.github.com>
2020-02-13 15:29:00 +0530

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,6 +1,6 @@
-n=int(input("Enter a integer: "))
-temp=str(n)
+num=int(input("Enter a integer: "))
+temp=str(num)
 t1=temp+temp
 t2=temp+temp+temp
-comp=n+int(t1)+int(t2)
+comp=num+int(t1)+int(t2)
 print("The value is:",comp)
